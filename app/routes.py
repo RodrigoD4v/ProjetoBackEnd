1c4bb71ec362f5f0bb0ee420b22a56c8f4609f28
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-05-31 18:43:39 -0300

    simplificação da da ia

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,8 @@ import numpy as np
 
 def import_model():
   modelo_path = os.path.abspath('./data/models/logistic_regression.pkl')
-  encoder_path = os.path.abspath('./data/models/encoder.pkl')
   scaler_path = os.path.abspath('./data/models/scaler.pkl')
   modelo = pickle.load(open(modelo_path, 'rb'))
-  encoder = pickle.load(open(encoder_path, 'rb'))
   scaler = pickle.load(open(scaler_path, 'rb'))
   return modelo, encoder, scaler
 
@@ -23,7 +21,6 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Obter os parâmetros do formulário
         age = float(request.form['age'])
         balance = float(request.form['balance'])
         duration = float(request.form['duration'])
@@ -31,31 +28,11 @@ def predict():
         pdays = float(request.form['pdays'])
         previous = float(request.form['previous'])
 
-        job = request.form['job']
-        marital = request.form['marital']
-        education = request.form['education']
-        default = request.form['default']
-        housing = request.form['housing']
-        loan = request.form['loan']
-        contact = request.form['contact']
-        month = request.form['month']
-        poutcome = request.form['poutcome']
-
-        # Codificar variáveis categóricas usando o encoder carregado
-        categorical_data = np.array([[job, marital, education, default, housing, loan, contact, month, poutcome]])
-        categorical_encoded = encoder.transform(categorical_data).toarray()
-
-        # Concatenar variáveis categóricas e numéricas
         numerical_data = np.array([[age, balance, duration, campaign, pdays, previous]])
-        parametros = np.hstack((categorical_encoded, numerical_data))
+        parametros_scaled = scaler.transform(numerical_data)
 
-        # Normalizar os dados
-        parametros_scaled = scaler.transform(parametros)
-
-        # Fazer a predição
         resultado = modelo.predict(parametros_scaled)[0]
 
-        # Interpretação do resultado
         if resultado == 'no':
             resultado_texto = 'Não há chances de subscrição'
         else:
